aefba8de90a755cae2729e507d1f9aa4c841615f
chaudhuree <chaudhuree00@gmail.com>
2023-06-20 22:44:10 +0600

    docs: question two solution done

--- module 3/assignment-03.js
+++ module 3/assignment-03.js
@@ -32,7 +32,6 @@ const dayOfWeek = getDayOfWeek(date);
 const currentDayOfWeek = getDayOfWeek();
 
 
-
 /*
 2.Math Object:
 
@@ -42,3 +41,20 @@ a. Write a JavaScript function that takes an array of numbers as input and retur
 
 b. Implement the function to display the square root of the sum of squares for an array of numbers.
 */
+
+// solution:
+
+function sqrRoot(numbers) {
+  const sumOfSqrNum = numbers.reduce((sum, num) => sum + Math.pow(num, 2), 0);
+  const sqrRootOfSum = Math.sqrt(sumOfSqrNum);
+  return sqrRootOfSum;
+}
+
+function displaySqrRootOfSumOfSqr(numbers) {
+  const result = sqrRoot(numbers);
+  console.log(`The given numbers are: ${numbers}`)
+  console.log(`The square root of the sum of squares is: ${result}`);
+}
+const num = [0, 3, 4];
+displaySqrRootOfSumOfSqr(num);
+
